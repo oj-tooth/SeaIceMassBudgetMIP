baf3050e1e0bf593c3866cfcc9fa11a56a37136b
Oliver Tooth <oliver.tooth@noc.ac.uk>
2026-04-10 10:28:39 +0100

    Update SIMBA run script and UKESM1-2-LL contribution, including Southern Hemisphere sea ice mass budget outputs.

--- contributions/UKESM1-2-LL/run_simba_UKESM1-2-LL.sh
+++ contributions/UKESM1-2-LL/run_simba_UKESM1-2-LL.sh
@@ -5,8 +5,7 @@ set -euo pipefail
 # run_simba_UKESM1-2-LL.sh
 #
 # Description: Run SeaIceMassBudgetAnalysis (SIMBA) to perform a
-# sea ice mass budget analysis from the UKESM1-2-LL esm-hist
-# simulation.
+# sea ice mass budget analysis from the UKESM1-2-LL simulations.
 #
 # SIMBA will be executed in the current process.
 #
@@ -19,13 +18,14 @@ config_file=./configs/UKESM1-2-LL_esm-up2p0-gwl-dn_config.toml
 log_file=./logs/UKESM1-2-LL_esm-up2p0-gwl-dn_simba.log
 
 # Run multiple pipelines:
-l_multi=false
+l_multi=true
 
 # Define Experiment IDs [l_multi=true] -> esm-up2p0-gwl
 # exp_ids=("esm-up2p0-gwl2p0" "esm-up2p0-gwl3p0" "esm-up2p0-gwl4p0")
 
 # Define Experiment IDs [l_multi=true] -> esm-up2p0-gwl-dn
-# exp_ids=("esm-up2p0-gwl2p0-50y-dn1p0" "esm-up2p0-gwl2p0-50y-dn2p0" "esm-up2p0-gwl3p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn1p0" "esm-up2p0-gwl4p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn2p0-gwl2p0")
+# exp_ids=("esm-up2p0-gwl2p0-50y-dn1p0" "esm-up2p0-gwl2p0-50y-dn2p0" "esm-up2p0-gwl3p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn2p0-gwl2p0")
+exp_ids=("esm-up2p0-gwl4p0-50y-dn1p0")
 
 # ================================== #
 
